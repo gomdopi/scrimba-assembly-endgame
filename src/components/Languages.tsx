d80ef54446a74134f9cf55f7412107ad4896978d
gomdopi <io.gomdopi@gmail.com>
2025-02-27 09:37:24 -0800

    scrimba: use ::before for skull icon on snapped languages

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -3,17 +3,13 @@ import { Language } from "../assets/data"
 
 export default function Languages(props: { languagesState: Array<Language> }) {
   const languagesElements = props.languagesState.map(language => (
-    <div key={language.name}>
-      <span
-        className={clsx(language.snapped && "snapped")}
-        style={language.styles}
-      >
-        {language.name}
-      </span>
-      <span className={clsx("snappedIcon", language.snapped && "show")}>
-        💀
-      </span>
-    </div>
+    <span
+      key={language.name}
+      className={clsx(language.snapped && "snapped")}
+      style={language.styles}
+    >
+      {language.name}
+    </span>
   ))
 
   return <div className="languagesContainer">{languagesElements}</div>

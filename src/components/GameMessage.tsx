e473767d2bb5762d98d393f2824426d9efa8ef22
gomdopi <io.gomdopi@gmail.com>
2025-02-28 08:24:41 -0800

    scrimba: pull "farewell language" message to utils.ts
    add more farewell phrases

--- src/components/GameMessage.tsx
+++ src/components/GameMessage.tsx
@@ -1,10 +1,11 @@
+import { getFarewellText } from "../utils"
+
 export default function GameMessage(props: { gameMessage: string }) {
-  const phrases = ["Farewell", "It's been real", "R.I.P."]
-  const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)]
-  const languageMessage =
+  const capitalizedLanguage =
     props.gameMessage.length > 0
-      ? `${randomPhrase}, ${props.gameMessage[0]}${props.gameMessage.slice(1)}`
+      ? props.gameMessage[0] + props.gameMessage.slice(1)
       : ""
+  const farewellMessage = getFarewellText(capitalizedLanguage)
 
   const generateMessage = () => {
     switch (props.gameMessage) {
@@ -23,7 +24,7 @@ export default function GameMessage(props: { gameMessage: string }) {
           </>
         )
       default: {
-        return <h4>{languageMessage}</h4>
+        return <h4>{farewellMessage}</h4>
       }
     }
   }
@@ -31,7 +32,7 @@ export default function GameMessage(props: { gameMessage: string }) {
   return (
     <div
       className={
-        languageMessage.length === 0
+        farewellMessage.length === 0
           ? "hide"
           : `gameMessage-${props.gameMessage}`
       }
